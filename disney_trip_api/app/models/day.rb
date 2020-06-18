9ec16c1ae88648d2b10310797e313a24c387f5fb
gavinsharkey <gavinsharkey2016@gmail.com>
2020-06-18 16:14:21 -0400

    Add dependent destroy for reservations in day model

--- disney_trip_api/app/models/day.rb
+++ disney_trip_api/app/models/day.rb
@@ -1,6 +1,6 @@
 class Day < ApplicationRecord
   belongs_to :trip
-  has_many :reservations
+  has_many :reservations, dependent: :destroy
   has_many :restaurants, through: :reservations, source: :reservable, source_type: "Restaurant"
   has_many :attractions, through: :reservations, source: :reservable, source_type: "Attraction"
 end
