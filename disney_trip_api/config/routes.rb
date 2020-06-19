bb814f6d2a36787f77da28db3de380a20cf4cd10
gavinsharkey <gavinsharkey2016@gmail.com>
2020-06-19 13:31:41 -0400

    Add index routes for resorts, parks Add nested index routes for restaurants, attractions

--- disney_trip_api/config/routes.rb
+++ disney_trip_api/config/routes.rb
@@ -4,5 +4,14 @@ Rails.application.routes.draw do
   end
 
   resources :days, only: [:show, :update, :destroy]
+
+  resources :resorts, only: [:index] do
+    resources :restaurants, only: [:index]
+  end
+
+  resources :parks, only: [:index] do
+    resources :restaurants, only: [:index]
+    resources :attractions, only: [:index]
+  end
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
