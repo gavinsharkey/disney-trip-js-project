960a06a37b8375aed100287fde0f530e2b28e1a7
gavinsharkey <gavinsharkey2016@gmail.com>
2020-06-19 09:10:45 -0400

    Add scope method by_created to Day add place method to Day

--- disney_trip_api/app/models/day.rb
+++ disney_trip_api/app/models/day.rb
@@ -3,4 +3,10 @@ class Day < ApplicationRecord
   has_many :reservations, dependent: :destroy
   has_many :restaurants, through: :reservations, source: :reservable, source_type: "Restaurant"
   has_many :attractions, through: :reservations, source: :reservable, source_type: "Attraction"
+
+  scope :by_created, -> { order(:created_at) }
+
+  def place
+    self.trip.days.by_created.index(self) + 1
+  end
 end
