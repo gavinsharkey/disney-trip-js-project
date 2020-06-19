c462c28dfd3586dd1a87db8c603931da438e3e69
gavinsharkey <gavinsharkey2016@gmail.com>
2020-06-19 13:00:07 -0400

    Add nested index, create routes for days add days resource

--- disney_trip_api/config/routes.rb
+++ disney_trip_api/config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
-  resources :trips
+  resources :trips do
+    resources :days, only: [:index, :create]
+  end
+
+  resources :days, only: [:show, :update, :destroy]
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
