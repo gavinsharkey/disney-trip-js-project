213017c7ca54610a22bb880365eaefc0e36c804c
gavinsharkey <gavinsharkey2016@gmail.com>
2020-06-20 14:32:17 -0400

    Update reservations create action

--- disney_trip_api/app/controllers/reservations_controller.rb
+++ disney_trip_api/app/controllers/reservations_controller.rb
@@ -1,7 +1,6 @@
 class ReservationsController < ApplicationController
   def create
-    day = Day.find_by(id: params[:day_id])
-    reservation = day.reservations.build(reservation_params)
+    reservation = Reservation.new(reservation_params)
     if reservation.save
       render json: reservation, include: {reservable: { only: [:name] } }, except: [:created_at, :updated_at]
     else
@@ -12,6 +11,6 @@ class ReservationsController < ApplicationController
   private
 
   def reservation_params
-    params.require(:reservation).permit(:reservable_id, :reservable_type, :time)
+    params.require(:reservation).permit(:day_id, :reservable_id, :reservable_type, :time)
   end
 end
