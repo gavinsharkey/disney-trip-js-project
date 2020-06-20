4d2cabf390e1f1721895ccac1ef7a1efe2940844
gavinsharkey <gavinsharkey2016@gmail.com>
2020-06-20 07:57:28 -0400

    Configure restaurants index action

--- disney_trip_api/app/controllers/restaurants_controller.rb
+++ disney_trip_api/app/controllers/restaurants_controller.rb
@@ -1,9 +1,13 @@
 class RestaurantsController < ApplicationController
   def index
     if params[:park_id]
-
+      park = Park.find_by(id: params[:park_id])
+      restaurants = park.restaurants
+      render json: restaurants, except: [:created_at, :updated_at]
     elsif params[:resort_id]
-
+      resort = Resort.find_by(id: params[:resort_id])
+      restaurants = resort.restaurants
+      render json: restaurants, except: [:created_at, :updated_at]
     end
   end
 end
