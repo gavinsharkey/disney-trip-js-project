11635908db21af26ff366a8be1c3cd4c4b19ba2f
gavinsharkey <gavinsharkey2016@gmail.com>
2020-06-20 13:54:59 -0400

    Reservation validates presence of and format of time

--- disney_trip_api/app/models/reservation.rb
+++ disney_trip_api/app/models/reservation.rb
@@ -1,4 +1,6 @@
 class Reservation < ApplicationRecord
   belongs_to :day, optional: true
   belongs_to :reservable, polymorphic: true
+
+  validates :time, presence: true, format: { with: /\w*:\w*/, message: 'must be time format' }
 end
