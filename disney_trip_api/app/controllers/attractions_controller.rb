ead3114baf134d819c1f3492cef73d3ab143f7c0
gavinsharkey <gavinsharkey2016@gmail.com>
2020-06-20 07:56:19 -0400

    Configure attractions index action

--- disney_trip_api/app/controllers/attractions_controller.rb
+++ disney_trip_api/app/controllers/attractions_controller.rb
@@ -1,4 +1,7 @@
 class AttractionsController < ApplicationController
   def index
+    park = Park.find_by(id: params[:park_id])
+    attractions = park.attractions
+    render json: attractions, except: [:created_at, :updated_at]
   end
 end
